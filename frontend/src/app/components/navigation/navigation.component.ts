52b89ce529a625f5d7206dd6081fa88feff3df74
Riad <reeaad999@hotmail.com>
2022-03-15 14:49:08 +0100

    changed the login to a popup

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
 import { AuthService } from "src/app/services/auth.service";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+
+import { SignupComponent } from "../signup/signup.component";
+import { LoginComponent } from "../login/login.component";
 
 @Component({
   selector: "app-navigation",
@@ -11,7 +16,12 @@ import { AuthService } from "src/app/services/auth.service";
 export class NavigationComponent implements OnInit {
   isAuthenticated = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public dialogRef: MatDialogRef<LoginComponent>,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.authService.isUserLoggedIn$.subscribe((isLoggedIn) => {
@@ -23,5 +33,27 @@ export class NavigationComponent implements OnInit {
     localStorage.removeItem("token");
     this.authService.isUserLoggedIn$.next(false);
     this.router.navigate(["login"]);
+  }
+  reloadPage(): void {
+    window.location.href = "/main-page";
+  }
+  onClose() {
+    this.dialogRef.close();
+  }
+  onSignUp() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "525px";
+    dialogConfig.height = "600px";
+    this.dialog.open(SignupComponent, dialogConfig);
+  }
+  onLogIn() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "525px";
+    dialogConfig.height = "600px";
+    this.dialog.open(LoginComponent, dialogConfig);
   }
 }
