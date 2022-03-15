82329fc2243e1fb6d75fec6cb2ad286820afe990
Riad <reeaad999@hotmail.com>
2022-03-15 13:54:03 +0100

    moved the app from gitlab to github

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+import { AuthService } from "src/app/services/auth.service";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+
+import { SignupComponent } from "../signup/signup.component";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    public dialogRef: MatDialogRef<LoginComponent>,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(7),
+      ]),
+    });
+  }
+
+  login(): void {
+    this.authService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe();
+  }
+  reloadPage(): void {
+    window.location.href = "/main-page";
+  }
+  onClose() {
+    this.dialogRef.close();
+  }
+  onSignUp() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "525px";
+    dialogConfig.height = "600px";
+    this.dialog.open(SignupComponent, dialogConfig);
+  }
+}
